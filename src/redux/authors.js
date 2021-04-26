ad7ac60f559ab6d03d601f6d1ac0013759141bea
Ivica Kartelo <ivicakartelo12@gmail.com>
2021-04-26 22:02:38 +0200

    combiningReducers

---
+++ src/redux/authors.js
@@ -0,0 +1,8 @@
+import { AUTHORS } from '../shared/authors';
+
+export const Authors = (state = AUTHORS, action) => {
+    switch (action.type) {
+        default:
+          return state;
+      }
+};
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,17 @@
-import {createStore} from 'redux';
-import { Reducer, initialState } from './reducer'
+import {createStore, combineReducers} from 'redux';
+import { Menus } from './menus';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+import { Authors } from './authors';
 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer, // reducer
-        initialState, // our initialState
+        combineReducers({
+            menus: Menus,
+            comments: Comments,
+            promotions: Promotions,
+            authors: Authors
+        })
     );
 
     return store;
